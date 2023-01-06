170f37c2eb26909bcdd9946f838139edc4bfa5df
Gon <hss3522@gmail.com>
2023-01-06 20:58:42 +0900

    Feat: 스크린 추가

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { ThemeProvider } from "@emotion/react";
-import { DarkTheme, DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+} from "@react-navigation/native";
 import { useColorScheme } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import Stacks from "./src/navigation/Stacks";
@@ -17,7 +22,9 @@ const App = () => {
       <Provider store={store}>
         <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
           <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
-            <Stacks />
+            <SafeAreaProvider>
+              <Stacks />
+            </SafeAreaProvider>
           </NavigationContainer>
         </ThemeProvider>
       </Provider>
--- src/navigation/Stacks.jsx
+++ src/navigation/Stacks.jsx
@@ -1,19 +1,22 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from '../screen/Home';
+import Detail from "../screen/Detail";
+import Home from "../screen/Home";
+import WriteList from "../screen/WriteList";
 
 const NativeStack = createNativeStackNavigator();
 
 const Stacks = () => {
   return (
-  <NativeStack.Navigator>
-    <NativeStack.Screen name="Home" component={Home} />
-    {/* <NativeStack.Screen name="Lists" />
-    <NativeStack.Screen name="Detail" />
+    <NativeStack.Navigator>
+      <NativeStack.Screen name="Detail" component={Detail} />
+      <NativeStack.Screen name="WriteList" component={WriteList} />
+      <NativeStack.Screen name="Home" component={Home} />
+      {/* <NativeStack.Screen name="Lists" />
     <NativeStack.Screen name="Mypage" />
     <NativeStack.Screen name="Login" />
     <NativeStack.Screen name="Register" /> */}
-  </NativeStack.Navigator>
-  )
+    </NativeStack.Navigator>
+  );
 };
 
 export default Stacks;
