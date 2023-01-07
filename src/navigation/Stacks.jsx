729cc5edece93de4ead45815a4f13a619e4bd3a5
JOCO <111356420+Jocooh@users.noreply.github.com>
2023-01-07 12:23:54 +0900

    [feat]Css,html 구조 만들기
    기본 틀 완성
    홈에서 list로 들어오는 라우터 확인
    (확인만하고 다시 지움)
    글쓰기 아이콘 누르면 글쓰는 공간으로 가는거 완료

--- src/navigation/Stacks.jsx
+++ src/navigation/Stacks.jsx
@@ -1,19 +1,26 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from '../screen/Home';
+import Home from "../screen/Home";
+import Lists from "../screen/Lists";
+import Detail from "../screen/Detail";
+import { headerBackVisible } from "react-native-screens";
 
 const NativeStack = createNativeStackNavigator();
 
 const Stacks = () => {
   return (
-  <NativeStack.Navigator>
-    <NativeStack.Screen name="Home" component={Home} />
-    {/* <NativeStack.Screen name="Lists" />
-    <NativeStack.Screen name="Detail" />
-    <NativeStack.Screen name="Mypage" />
+    <NativeStack.Navigator>
+      {/* <NativeStack.Screen name="Home" component={Home} /> */}
+      <NativeStack.Screen
+        name="Lists"
+        component={Lists}
+        options={headerBackVisible}
+      />
+      {/* <NativeStack.Screen name="Detail" component={Detail} /> */}
+      {/* <NativeStack.Screen name="Mypage" />
     <NativeStack.Screen name="Login" />
     <NativeStack.Screen name="Register" /> */}
-  </NativeStack.Navigator>
-  )
+    </NativeStack.Navigator>
+  );
 };
 
 export default Stacks;
