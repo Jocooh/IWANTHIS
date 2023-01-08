6d48faaac091fc6a736aa970c5ce4b3596387743
freesian <zzte123@naver.com>
2023-01-08 21:11:49 +0900

    [feat] 슬라이드 효과 및 디자인 재구성

--- src/navigation/Stacks.jsx
+++ src/navigation/Stacks.jsx
@@ -5,7 +5,9 @@ import Detail from "../screen/Detail";
 import WriteList from "../screen/WriteList";
 import { headerBackVisible } from "react-native-screens";
 import Header from "../components/Header";
+import { imagePath } from "../assets/imgPath";
 import { Text, Image, TouchableOpacity, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 const NativeStack = createNativeStackNavigator();
 
@@ -13,6 +15,7 @@ const Stacks = () => {
   return (
     <NativeStack.Navigator
       screenOptions={{
+        headerTitleAlign: "center", // 안드로이드
         headerTitle: (props) => <Header {...props} />,
         headerLeft: () => <Text></Text>, // 물어보깅
         headerRight: () => {
@@ -20,7 +23,7 @@ const Stacks = () => {
             <TouchableOpacity>
               <View style={{ marginVertical: -5 }}>
                 <Image
-                  source={require("../assets/defaultimage.png")}
+                  source={imagePath["defaultimage"]}
                   style={{ height: 40, width: 40 }}
                 />
                 {/* <View style={{ height: 10 }}></View> */}
@@ -37,9 +40,9 @@ const Stacks = () => {
         options={headerBackVisible}
       />
       <NativeStack.Screen name="Detail" component={Detail} />
-      <NativeStack.Screen name="WriteList" component={WriteList} /> 
+      <NativeStack.Screen name="WriteList" component={WriteList} />
       {/* <NativeStack.Screen name="Mypage" /> */}
-      {/* <NativeStack.Screen name="Login" /> */}
+      <NativeStack.Screen name="Login" component={WriteList} />
       {/* <NativeStack.Screen name="Register" />*/}
     </NativeStack.Navigator>
   );
