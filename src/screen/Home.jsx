8d06777b5a8b999726f264a0788476c44569420d
Gon <hss3522@gmail.com>
2023-01-12 23:48:06 +0900

    Feat: Login Check

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,8 +1,25 @@
 import { ScrollView } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Category from "../components/Category";
+import { auth } from '../common/firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { isLogin, notLogin } from '../redux/modules/loginSlice';
 
 const Home = ({ navigation: { navigate } }) => {
+  const dispatch = useDispatch();
+  const checkLogin = useSelector(state => state.login.isLogin)
+  console.log(checkLogin);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(isLogin())
+      } else {
+        dispatch(notLogin())
+      }
+    });
+  }, []);
+
   return (
     <ScrollView>
       <Category navigation={navigate} />
